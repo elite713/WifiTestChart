a032b1dddb56f80d35d60c8d0b46f01d4ec1bf23
Renata RR <elite713@hotmail.com>
2020-07-20 11:16:56 -0500

    Adding path for the json data and adding frecuency table for the 5Ghz band.

--- src/page/wifi-widget.container.js
+++ src/page/wifi-widget.container.js
@@ -3,6 +3,10 @@ import './wifi-widget.container.css'
 import KeyWords from './keywords'
 import TestRun from './test-run'
 import FrecuencyTable from './frecuency-table'
+import WifiRecomendation from './data/wifi-recommendation'
+
+const preferencesList0 = WifiRecomendation.wifi_recommendations[0].locations[0].cpe_devices[0].recommendations[0].preferences;
+const preferencesList1 = WifiRecomendation.wifi_recommendations[0].locations[0].cpe_devices[0].recommendations[1].preferences;
 
 class WifiWidgetContainer extends Component {
   render() {
@@ -15,8 +19,8 @@ class WifiWidgetContainer extends Component {
             <div className="wifi-grid-reload-2"> Reload </div>
             <div className="wifi-grid-test-3"> Test Return <TestRun/> </div>
             <div className="wifi-grid-keywords-4"> <KeyWords/> </div>
-            <div className="wifi-grid-band-24-5"> Frecuency Band 2.4 GHz <FrecuencyTable/></div>
-            <div className="wifi-grid-band-50-6"> Frecuency Band 5 GHz </div>
+            <div className="wifi-grid-band-24-5"> Frecuency Band 2.4 GHz <FrecuencyTable preferencesList={preferencesList1}  /></div>
+            <div className="wifi-grid-band-50-6"> Frecuency Band 5 GHz <FrecuencyTable preferencesList={preferencesList0}  /></div>
             <div className="wifi-grid-band-info-7"> Frecuency band informatiom </div>
             <div className="wifi-grid-buttom-8"> Bottoms Box </div>
           </div>    

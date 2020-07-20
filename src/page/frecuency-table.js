e05c62ff3955a367379f79313c6af0f9faf83309
Renata RR <elite713@hotmail.com>
2020-07-20 11:00:43 -0500

    Component improvement to make it generic to reuse it for 5 GH band.

--- src/page/frecuency-table.js
+++ src/page/frecuency-table.js
@@ -1,18 +1,14 @@
 import React, { Component} from 'react'
-import WifiRecomendation from './data/wifi-recommendation'
 import FrecuencyCell from './frecuency-cell'
 import './frecuency-table.css'
 
-const preferencesList = WifiRecomendation.wifi_recommendations[0].locations[0].cpe_devices[0].recommendations[1].preferences;
 
 class FrecuencyTable extends Component {
     render() {
-        const cellResult= [];
-          for (var i=1; i<=13; i++){
-              var value = preferencesList[""+i];
-          cellResult.push(<div className="cell-1"><FrecuencyCell cellColorStyle={value} label={""+i}/></div>);  
-          }
-
+      const cellResult= [];
+      Object.keys(this.props.preferencesList).forEach(function(key){
+        cellResult.push(<div className="cell-1"><FrecuencyCell cellColorStyle={this.props.preferencesList[key]} label={key}/></div>);
+      },this)
         return(
            <div>
                  <div className="wrapperfr2">

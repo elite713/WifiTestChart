7f3be6ce966ee07abb427ccc845b89c27a705515
Renata RR <elite713@hotmail.com>
2020-07-22 16:31:36 -0500

    Adding a new full Json Data and changing the test run component pasing the Json data to the dropdown

--- src/page/test-run.js
+++ src/page/test-run.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react'
 import './test-run.css'
+import dataModel from './data/data_model'
 
 class TestRun extends Component {
     render() {
+        var optionsResult=[];
+        
+        dataModel.data_model_v2.cpe_recommendations.sessions.map(item=>{
+
+            var convert_optionsResult;
+            var currentDate;
+            if (item.timestamp != 0)
+            convert_optionsResult = new Date(item.timestamp).toISOString();
+            else 
+            convert_optionsResult = "current session";
+            optionsResult.push(<option value={item.sessionId}>{convert_optionsResult}</option>);
+
+        })
+
         return(
             <div className="test-return">
                <select className="test-session-list" id="session">
-                   <option value="option1">01/01/2018 12:01:35</option>
-                   <option value="option2">01/02/2018 12:01:35</option>
-                   <option value="option3">01/03/2018 12:01:35</option>
-                   <option value="option4">01/04/2018 12:01:35</option>
-                   <option value="option5">01/05/2018 12:01:35</option>
-                   <option value="option6">01/06/2018 12:01:35</option>
-                   <option value="option7">01/07/2018 12:01:35</option>               
+                    {optionsResult}
                </select>
 
             </div>

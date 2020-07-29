651ce514258849ac856b7969b4452705eb5fd25a
Renata RR <elite713@hotmail.com>
2020-07-29 18:51:35 -0500

    Updating the frecuency bands when a session is selected.

--- src/page/test-run.js
+++ src/page/test-run.js
@@ -3,24 +3,29 @@ import './test-run.css'
 import dataModel from './data/data_model'
 
 class TestRun extends Component {
+
+
     render() {
         var optionsResult=[];
         
         dataModel.data_model_v2.cpe_recommendations.sessions.map(item=>{
 
             var convert_optionsResult;
-            var currentDate;
-            if (item.timestamp != 0)
-            convert_optionsResult = new Date(item.timestamp).toISOString();
-            else 
-            convert_optionsResult = "current session";
+
+            if (item.timestamp != 0) {
+                convert_optionsResult = new Date(item.timestamp).toISOString();
+            } else { 
+                convert_optionsResult = "current session";
+            }
+
+
             optionsResult.push(<option value={item.sessionId}>{convert_optionsResult}</option>);
 
         })
-
+         
         return(
             <div className="test-return">
-               <select className="test-session-list" id="session">
+               <select className="test-session-list" id="session" onChange={this.props.updateSession}>
                     {optionsResult}
                </select>
 
--- src/page/wifi-widget.container.js
+++ src/page/wifi-widget.container.js
@@ -3,12 +3,50 @@ import './wifi-widget.container.css'
 import KeyWords from './keywords'
 import TestRun from './test-run'
 import FrecuencyTable from './frecuency-table'
-import WifiRecomendation from './data/wifi-recommendation'
+import DataModel from './data/data_model'
 
-const preferencesList0 = WifiRecomendation.wifi_recommendations[0].locations[0].cpe_devices[0].recommendations[0].preferences;
-const preferencesList1 = WifiRecomendation.wifi_recommendations[0].locations[0].cpe_devices[0].recommendations[1].preferences;
+//const preferencesList0 = WifiRecommendation.wifi_recommendations[0].locations[0].cpe_devices[0].recommendations[0].preferences;
+//const preferencesList1 = DataModel.data_model_v2.wifi_recommendations[0].    locations[0].cpe_devices[0].recommendations[1].preferences;
 
 class WifiWidgetContainer extends Component {
+  constructor (props){
+    super(props)
+
+    this.state= {
+      selectedSessionId: "",
+      wifiRecommendation: DataModel.data_model_v2.wifi_recommendations[0]
+    }
+
+    this.refreshSession = this.refreshSession.bind(this);
+    this.updateRecommendation = this.updateRecommendation.bind(this);
+
+  }
+
+  refreshSession(event) {
+    console.log("from parent : " + event.target.value)
+    this.setState({
+      selectedSessionId: event.target.value      
+    },this.updateRecommendation)
+
+    //this.updateRecommendation();
+  }
+
+  updateRecommendation(){
+
+    console.log("updateRecomendation: " + this.state.selectedSessionId);
+
+    let indiceResult = DataModel.data_model_v2.wifi_recommendations.filter(recommendationItem => {
+      
+      return this.state.selectedSessionId == recommendationItem.sessionId
+
+    })
+
+
+    this.setState({
+      wifiRecommendation: indiceResult[0]
+    })
+  }
+
   render() {
     
     return (
@@ -17,10 +55,10 @@ class WifiWidgetContainer extends Component {
           <div className="wifi-grid-wrapper" >                
             <div className="wifi-grid-title-1">Wifi Diagnostics Test </div>
             <div className="wifi-grid-reload-2"> Reload </div>
-            <div className="wifi-grid-test-3"> Test Return <TestRun/> </div>
+            <div className="wifi-grid-test-3"> Test Return <TestRun updateSession={this.refreshSession}/> </div>
             <div className="wifi-grid-keywords-4"> <KeyWords/> </div>
-            <div className="wifi-grid-band-24-5"> Frecuency Band 2.4 GHz <FrecuencyTable preferencesList={preferencesList1}  /></div>
-            <div className="wifi-grid-band-50-6"> Frecuency Band 5 GHz <FrecuencyTable preferencesList={preferencesList0}  /></div>
+            <div className="wifi-grid-band-24-5"> Frecuency Band 2.4 GHz <FrecuencyTable preferencesList={this.state.wifiRecommendation.locations[0].cpe_devices[0].recommendations[1].preferences}  /></div>
+            <div className="wifi-grid-band-50-6"> Frecuency Band 5 GHz <FrecuencyTable preferencesList ={this.state.wifiRecommendation.locations[0].cpe_devices[0].recommendations[0].preferences} /></div>
             <div className="wifi-grid-band-info-7"> Frecuency band informatiom </div>
             <div className="wifi-grid-buttom-8"> Bottoms Box </div>
           </div>    
